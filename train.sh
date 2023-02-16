a254bf6a9ebe4da887197178e0712084eba750d7
DominikKwiatkowski <domik.rex@gmail.com>
2023-02-16 18:08:19 +0100

    Change training plan.

--- train.sh
+++ train.sh
@@ -10,31 +10,14 @@ nohup $( \
        ) &
 
 nohup $( \
-        python ./src/run_training.py --ww=225 --wl=25 --multiclass --discard --batch_size=4 --epochs=50 --gpu=4 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=quanet --loss_name=cross_entropy &>> global.log; \
-        python ./src/run_training.py --ww=225 --wl=25 --multiclass --discard --batch_size=8 --epochs=50 --gpu=4 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unet --loss_name=cross_entropy &>> global.log; \
-        python ./src/run_training.py --ww=225 --wl=25 --multiclass --discard --batch_size=4 --epochs=50 --gpu=4 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unetplusplus --loss_name=cross_entropy &>> global.log \
+        python ./src/run_training.py --ww=225 --wl=25 --tumor --discard --batch_size=8 --epochs=50 --gpu=4 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unet --loss_name=mix &>> global.log; \
+        python ./src/run_training.py --ww=225 --wl=25 --tumor --discard --batch_size=4 --epochs=50 --gpu=4 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=quanet --loss_name=mix &>> global.log; \
+        python ./src/run_training.py --ww=225 --wl=25 --tumor --discard --batch_size=4 --epochs=50 --gpu=4 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unetplusplus --loss_name=mix &>> global.log \
        ) &
 
 nohup $( \
-        python ./src/run_training.py --ww=225 --wl=25 --tumor --discard --batch_size=8 --epochs=50 --gpu=5 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unet --loss_name=mix &>> global.log; \
-        python ./src/run_training.py --ww=225 --wl=25 --tumor --discard --batch_size=4 --epochs=50 --gpu=5 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=quanet --loss_name=mix &>> global.log; \
-        python ./src/run_training.py --ww=225 --wl=25 --tumor --discard --batch_size=4 --epochs=50 --gpu=5 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unetplusplus --loss_name=mix &>> global.log \
+        python ./src/run_training.py --ww=225 --wl=25 --discard --batch_size=8 --epochs=50 --gpu=5 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unet --loss_name=mix &>> global.log; \
+        python ./src/run_training.py --ww=225 --wl=25 --discard --batch_size=4 --epochs=50 --gpu=5 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=quanet --loss_name=mix &>> global.log; \
+        python ./src/run_training.py --ww=225 --wl=25 --discard --batch_size=4 --epochs=50 --gpu=5 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unetplusplus --loss_name=mix &>> global.log \
        ) &
 
-nohup $( \
-        python ./src/run_training.py --ww=225 --wl=25 --tumor --discard --batch_size=4 --epochs=50 --gpu=6 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=quanet --loss_name=cross_entropy &>> global.log; \
-        python ./src/run_training.py --ww=225 --wl=25 --tumor --discard --batch_size=8 --epochs=50 --gpu=6 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unet --loss_name=cross_entropy &>> global.log; \
-        python ./src/run_training.py --ww=225 --wl=25 --tumor --discard --batch_size=4 --epochs=50 --gpu=6 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unetplusplus --loss_name=cross_entropy &>> global.log \
-       ) &
-
-nohup $( \
-        python ./src/run_training.py --ww=225 --wl=25 --discard --batch_size=8 --epochs=50 --gpu=1 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unet --loss_name=mix &>> global.log; \
-        python ./src/run_training.py --ww=225 --wl=25 --discard --batch_size=4 --epochs=50 --gpu=1 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=quanet --loss_name=mix &>> global.log; \
-        python ./src/run_training.py --ww=225 --wl=25 --discard --batch_size=4 --epochs=50 --gpu=1 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unetplusplus --loss_name=mix &>> global.log \
-       ) &
-
-nohup $( \
-        python ./src/run_training.py --ww=225 --wl=25 --discard --batch_size=4 --epochs=50 --gpu=2 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=quanet --loss_name=cross_entropy &>> global.log; \
-        python ./src/run_training.py --ww=225 --wl=25 --discard --batch_size=8 --epochs=50 --gpu=2 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unet --loss_name=cross_entropy &>> global.log; \
-        python ./src/run_training.py --ww=225 --wl=25 --discard --batch_size=4 --epochs=50 --gpu=2 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unetplusplus --loss_name=cross_entropy &>> global.log \
-       ) &
