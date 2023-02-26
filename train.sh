c88c8058280be29bcb53d4421789677db45b3eb8
DominikKwiatkowski <domik.rex@gmail.com>
2023-02-26 23:18:13 +0100

    minr fix in plan.

--- train.sh
+++ train.sh
@@ -15,7 +15,7 @@ nohup $( \
       ) &
 
 nohup $( \
-        python ./src/run_training.py --no_val --ww=225 --wl=25 --multiclass --discard --batch_size=8 --epochs=50 --gpu=2--fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unet --loss_name=mix --seed=32 &>> global.log; \
+        python ./src/run_training.py --no_val --ww=225 --wl=25 --multiclass --discard --batch_size=8 --epochs=50 --gpu=2 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unet --loss_name=mix --seed=32 &>> global.log; \
         python ./src/run_training.py --no_val --ww=225 --wl=25 --use_polar --multiclass --discard --batch_size=8 --epochs=50 --gpu=2 --fold=0 --learning_rate=0.0001 --img_size=512 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unet --loss_name=mix --seed=32 &>> global.log; \
         python ./src/run_training.py --no_val --ww=225 --wl=25 --discard --batch_size=32 --epochs=50 --gpu=2 --fold=0 --learning_rate=0.0001 --img_size=256 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unet --loss_name=mix --seed=30 &>> global.log; \
         python ./src/run_training.py --no_val --ww=225 --wl=25 --discard --batch_size=32 --epochs=50 --gpu=2 --fold=0 --learning_rate=0.0001 --img_size=256 --metadata="/home/macierz/s175454/lits_prepared/metadata.csv" --dataset="/home/macierz/s175454/lits_prepared" --net_name=unet --loss_name=mix --seed=31 &>> global.log \
