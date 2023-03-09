92c2d3e5a1a9ca0e6135fc3780055960fbdf2713
DominikKwiatkowski <domik.rex@gmail.com>
2023-03-09 18:39:06 +0100

    Change epochs in defednet.

--- src/data_loader.py
+++ src/data_loader.py
@@ -162,7 +162,7 @@ class TomographyDataset(Dataset):
             sampler = SubsetSequentialSampler(slice_ids)
 
         data_loader = DataLoader(
-            self, batch_size=batch_size, sampler=sampler, num_workers=2
+            self, batch_size=batch_size, sampler=sampler, num_workers=4
         )
         return data_loader
 
